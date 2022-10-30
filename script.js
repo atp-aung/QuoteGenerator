78d40e41fa5459ead20e70237b22b935eeb0151c
atp <atp.aung.5555@gmail.com>
2022-10-30 07:41:10 +0630

    a

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hello saturday");
+let quoData = [];
+async function quoteGentor() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    let respon = await fetch(apiUrl);
+    quoData = await respon.json();
+    console.log(quoData[Math.trunc(Math.random() * quoData.length)]);
+  } catch (er) {
+    console.log(er);
+  }
+}
+quoteGentor();
