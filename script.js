c4c7d949f73ac9e34f3b675d31af202dc292f5cc
atp <atp.aung.5555@gmail.com>
2022-11-03 21:46:12 +0630

    f

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 "use strict";
+let newQuote = document.getElementById("new-quote");
+let quoteHtml = document.getElementById("quote");
+let author = document.getElementById("author");
+let quotContanr = document.getElementById("quot-conta");
+let loader = document.getElementById("loader");
 let quoData = [];
-let randQuot = () => {
-  let quot = quoData[Math.trunc(Math.random() * quoData.length)];
-  console.log(quot);
+let randQot = () => {
+  loading();
+  let quote = quoData[Math.floor(Math.random() * quoData.length)];
+  quoteHtml.textContent = quote.text;
+  author.textContent = quote.author;
+  dataShow();
+};
+let loading = () => {
+  loader.hidden = false;
+  quotContanr.hidden = true;
+};
+let dataShow = () => {
+  loader.hidden = true;
+  quotContanr.hidden = false;
 };
 async function quoteGentor() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     let respon = await fetch(apiUrl);
     quoData = await respon.json();
-    randQuot();
+    randQot();
   } catch (er) {
     console.log(er);
   }
 }
 quoteGentor();
+newQuote.addEventListener("click", randQot);
